28186c8766843c77510050ed842dc2c5a92d7fc5
mparulski <StrongerCodeForAllPeople103>
2021-05-19 22:08:42 +0200

    feat: add typescript

--- src/scripts/modules.js
+++ src/scripts/modules.js
@@ -1,8 +1,9 @@
 module.exports = {
-  BABEL: "babel",
-  ESLINT: "eslint",
-  JEST: "jest",
-  PRETTIER: "prettier",
-  REACT: "react",
-  WEBPACK: "webpack",
-};
+  BABEL: 'babel',
+  ESLINT: 'eslint',
+  JEST: 'jest',
+  PRETTIER: 'prettier',
+  REACT: 'react',
+  TYPESCRIPT: 'typescript',
+  WEBPACK: 'webpack',
+}
