cb6a60920ecf06029938503ea8db2ecc994a13ca
Jan Latoch <jan.latoch@gmail.com>
2021-06-12 16:35:12 +0200

    feat: upgrade webpack and babel configs

--- src/config/babel/babel-react.config.js
+++ src/config/babel/babel-react.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic',
-      },
+      '@babel/preset-react'
     ],
   ],
 }
--- src/packages/webpack.js
+++ src/packages/webpack.js
@@ -2,6 +2,8 @@ module.exports = {
     base: [
         {"webpack": "5.37.1"},
         {"webpack-merge": "5.7.3"},
-        {"@mparulski/es-project-maker-webpack": "0.0.1-alpha.3"},
+        {"webpack-cli": "4.7.0"},
+        {"webpack-dev-server": "3.11.2"},
+        {"@mparulski/es-project-maker-webpack": "0.0.1-beta.1"},
     ]
 }
