55127e911f8d833419d4679b9c21e23e81d3d978
mparulski <michal.parulski@gmail.com>
2021-06-16 21:39:24 +0200

    feat: upgrade @mparulski/es-project-maker-webpack

--- src/packages/webpack.js
+++ src/packages/webpack.js
@@ -2,6 +2,6 @@ module.exports = {
   base: [
     {webpack: '5.39.0'},
     {'webpack-merge': '5.8.0'},
-    {'@mparulski/es-project-maker-webpack': '0.0.1-beta.1'},
+    {'@mparulski/es-project-maker-webpack': '0.0.1-beta.2'},
   ],
 }
