b132e029c4a5f6aa79cf49a1f7f798cd7e6fce5e
mparulski <michal.parulski@gmail.com>
2021-06-18 05:31:42 +0200

    feat: update eslint deps

--- src/config/eslint/eslint.config.js
+++ src/config/eslint/eslint.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  extends: [].map(require.resolve),
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  ...require('@mparulski/es-project-eslint/rules/errors'),
-}
+module.exports = require('@mparulski/es-project-eslint/eslint.config')
