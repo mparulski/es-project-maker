38896f7be40356c79936ad262ec5ed914a6b2189
mparulski <michal.parulski@gmail.com>
2021-08-28 20:45:04 +0200

    feat: update deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## 0.5.0
+- set babel & typescript as default modules (#7)
+- update @mparulski/es-project-maker-webpack to 0.5.0
+- update typescript to 4.4.2
+
 ## 0.4.0
 - update webpack to 5.50.1
 - update webpack-cli to 4.8.0
--- src/packages/typescript.js
+++ src/packages/typescript.js
@@ -1,6 +1,6 @@
 module.exports = {
   base: [
-    {typescript: {version: '4.3.5', type: '--save-dev'}},
+    {typescript: {version: '4.4.2', type: '--save-dev'}},
     {'ts-loader': {version: '9.2.5', type: '--save-dev'}},
     {'@types/react': {version: '17.0.19', type: '--save-dev'}},
     {'@types/react-dom': {version: '17.0.9', type: '--save-dev'}},
